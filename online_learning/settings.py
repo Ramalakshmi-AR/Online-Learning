02decc04e66309efa8e14d7f27779d35a555b5e9
Ramalakshmi-AR <ramyarani.a@gmail.com>
2025-12-26 21:02:06 +0530

    Fix static files and prepare for Render deployment

--- online_learning/settings.py
+++ online_learning/settings.py
@@ -24,12 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6%lo6=rns^77gj+c0j75=ji6-k^5=vi+!ot$(4e35(t!&x4qu0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,12 +117,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-RAZORPAY_KEY_ID = 'rzp_test_RgQ7aqPF3uAVyp'
-RAZORPAY_KEY_SECRET = '5Ktz63WjFVnzVBfhAXgNWCHR'
+# RAZORPAY_KEY_ID = 'rzp_test_RgQ7aqPF3uAVyp'
+# RAZORPAY_KEY_SECRET = '5Ktz63WjFVnzVBfhAXgNWCHR'
+
+
+
+RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
+RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
 
 
 LOGIN_URL = 'login'      
